ab1bf624e6d0ea02d9800789c091b15eaeaa6b93
Muhammad Rezky Aulia Saputra <rezkyaulia907@gmail.com>
2025-12-01 00:41:45 +0800

    add login screen and profile screen

--- lib/views/login_screen.dart
+++ lib/views/login_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:laporin_app/views/profile_google_screen.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -7,151 +9,171 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              "Welcome",
-              style: GoogleFonts.inter(
-                fontSize: 33,
-                fontWeight: FontWeight.w600,
-                color: Color(0xFF4C4B4B),
-              ),
-            ),
-            Text(
-              "Aplikasi Cerdas untuk Masyarakat Melapor Kejadian.",
-              style: GoogleFonts.inter(
-                fontSize: 15,
-                fontWeight: FontWeight.normal,
-                color: Color(0xFF4C4B4B),
-              ),
-              textAlign: TextAlign.center,
-            ),
-            TextField(
-              textAlign: TextAlign.start,
-              autofocus: true,
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: const Color(0xFFEFEFEF), // warna background
-                hintText: "Search movies...",
-                hintStyle: const TextStyle(
-                  color: Color(0xFF4C4B4B),
-                  fontSize: 14,
-                  fontWeight: FontWeight.normal,
-                  decoration: TextDecoration.none,
-                ),
-                contentPadding: const EdgeInsets.symmetric(
-                  vertical: 9,
-                  horizontal: 12,
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide.none,
-                ),
-              ),
-              style: const TextStyle(
-                color: Color(0xFF4C4B4B),
-                fontSize: 14,
-                fontWeight: FontWeight.normal,
-                decoration: TextDecoration.none,
-              ),
-            ),
-            TextField(
-              textAlign: TextAlign.start,
-              autofocus: true,
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: const Color(0xFFEFEFEF), // warna background
-                hintText: "Search movies...",
-                hintStyle: const TextStyle(
-                  color: Color(0xFF4C4B4B),
-                  fontSize: 14,
-                  fontWeight: FontWeight.normal,
-                  decoration: TextDecoration.none,
-                ),
-                contentPadding: const EdgeInsets.symmetric(
-                  vertical: 9,
-                  horizontal: 12,
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide.none,
-                ),
-              ),
-              style: const TextStyle(
-                color: Color(0xFF4C4B4B),
-                fontSize: 14,
-                fontWeight: FontWeight.normal,
-                decoration: TextDecoration.none,
-              ),
-            ),
-            Align(
-              alignment: Alignment.centerRight,
-              child: Text(
-                "Forgot Password?",
-                style: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.normal,
-                  color: Color(0xFF4C4B4B),
-                ),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(vertical: 14, horizontal: 32),
-                backgroundColor: Color(0xFF4C4B4B),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-              child: Text(
-                "Login",
-                style: TextStyle(fontSize: 16, color: Colors.white),
-              ),
-            ),
-            Text(
-              "Or continue with",
-              style: TextStyle(
-                fontSize: 15,
-                fontWeight: FontWeight.normal,
-                color: Color(0xFF4C4B4B),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(vertical: 14, horizontal: 32),
-                backgroundColor: Color(0xFF4C4B4B),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-              child: Text(
-                "Google",
-                style: TextStyle(fontSize: 16, color: Colors.white),
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Don't have an account?"),
-                TextButton(
-                  style: TextButton.styleFrom(
-                    padding: EdgeInsets.zero, // hilangkan padding default
+      body: SafeArea(
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            return SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(minHeight: constraints.maxHeight),
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Text(
+                        "Welcome",
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.inter(
+                          fontSize: 33,
+                          fontWeight: FontWeight.w600,
+                          color: Color(0xFF0F55C7),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        "Aplikasi Cerdas untuk Masyarakat Melapor Kejadian.",
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.inter(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w400,
+                          color: Color(0xFF4C4B4B),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      TextField(
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Color(0xFFEBEAEA),
+                          hintText: "Email...",
+                          contentPadding: EdgeInsets.symmetric(
+                            vertical: 20,
+                            horizontal: 12,
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide.none,
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(color: Color(0xFF0F55C7)),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Color(0xFFEBEAEA),
+                          hintText: "Password...",
+                          contentPadding: EdgeInsets.symmetric(
+                            vertical: 20,
+                            horizontal: 12,
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide.none,
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(color: Color(0xFF0F55C7)),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: Text(
+                          "Forgot Password?",
+                          style: TextStyle(
+                            fontSize: 13,
+                            fontWeight: FontWeight.w200,
+                            color: Color(0xFF4C4B4B).withValues(alpha: 0.8),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (_) => const ProfileGoogleScreen(),
+                              ),
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            padding: EdgeInsets.symmetric(
+                              vertical: 14,
+                              horizontal: 32,
+                            ),
+                            backgroundColor: Color(0xFF0F55C7),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            elevation: 3,
+                          ),
+                          child: Text(
+                            "Sign In",
+                            style: TextStyle(fontSize: 16, color: Colors.white),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      Text(
+                        "Or continue with",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Color(0xFF4C4B4B),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      ElevatedButton.icon(
+                        onPressed: () {},
+                        icon: SvgPicture.asset(
+                          "assets/images/login_screen/google.svg",
+                        ),
+                        label: Text(
+                          "Google",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Color(0xFF4C4B4B),
+                          ),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xFFEBEBEB),
+                          padding: EdgeInsets.symmetric(vertical: 14),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("Don't have an account?"),
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              "Sign Up",
+                              style: TextStyle(color: Color(0xFF0F55C7)),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                    ],
                   ),
-                  onPressed: () {},
-                  child: Text(
-                    "Sign Up",
-                    style: TextStyle(color: Color(0xFF0F55C7)),
-                  ),
                 ),
-              ],
-            ),
-          ],
+              ),
+            );
+          },
         ),
       ),
     );
